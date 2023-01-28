092324efc20c6f3df8e3a22f20d78818671fe870
matheusjustino <matheus.justino@ccc.ufcg.edu.br>
2023-01-28 12:14:12 -0300

    small change

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -57,7 +57,7 @@ export default class MyDocument extends Document {
 					<link
 						rel="preconnect"
 						href="https://fonts.gstatic.com"
-						crossOrigin={'true'}
+						crossOrigin=""
 					/>
 					<link
 						href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;700&display=swap"
